8cd6f79b700b59eea9eaaf51dc8f04f294d27ae8
Guillaume Richard <guitch95@gmail.com>
2026-06-01 13:40:29 +0200

    added sf symbols

--- CarnetDeVoyage/ContentView.swift
+++ CarnetDeVoyage/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.blue.opacity(0.6)
+            Color.blue.opacity(0.8)
                 .ignoresSafeArea()
             VStack {
                 Text("Mon carnet de voyage")
                     .font(.title)
-                Spacer()
+                    .foregroundStyle(.white)
+                
+                HStack(spacing: 20) {
+                    Image(systemName: "globe")
+                    Image(systemName: "airplane")
+                    Image(systemName: "globe.europe.africa.fill")
+                }
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .padding(.top)
             }
             .padding()
         }
