ba9f1bcb64007d6096981b1691486bd101308743
Guillaume Richard <guitch95@gmail.com>
2026-06-01 13:29:21 +0200

    initial commit

---
+++ CarnetDeVoyage/CarnetDeVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnetDeVoyageApp.swift
+//  CarnetDeVoyage
+//
+//  Created by Guillaume Richard on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarnetDeVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
